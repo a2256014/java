0636fcd26870119e2139edf061a2a59f35a8a71f
dogyun <a2256014@naver.com>
2022-06-02 20:23:24 +0900

    rest

--- programmers/javaShcool/Step_1_2.java
+++ programmers/javaShcool/Step_1_2.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 public class Step_1_2 {
-    public static void main(String[] args){
-        String result = solution(new int[] {30,34,3,3,3,31,1,9});
+    public static void main(String[] args) {
+        String result = solution(new int[]{30, 34, 3, 3, 3, 31, 1, 9});
         System.out.println(result);
     }
-    public static String solution(int[] numbers){
+
+    public static String solution(int[] numbers) {
         String answer = "";
         String[] num = new String[numbers.length];
-        int index=0;
-        for(int number : numbers){
-            num[index++]=number+"";
+        int index = 0;
+        for (int number : numbers) {
+            num[index++] = number + "";
         }
         Arrays.sort(num, (o1, o2) -> {
-            if(o1.length()==o2.length()) return o2.compareTo(o1);
-            else return (o2+o1).compareTo(o1+o2);
+            if (o1.length() == o2.length()) return o2.compareTo(o1);
+            else return (o2 + o1).compareTo(o1 + o2);
         });
         System.out.println(Arrays.toString(num));
 
-        for(String n : num){
+        for (String n : num) {
             answer = answer.concat(n);
         }
-        if(answer.charAt(0)=='0') return "0";
+        if (answer.charAt(0) == '0') return "0";
         return answer;
     }
 }
