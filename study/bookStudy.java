b858250889d6632e86dc1debd22e105fd20bb4f3
dogyun <a2256014@naver.com>
2022-06-02 20:18:51 +0900

    2022/6/2

--- study/bookStudy.java
+++ study/bookStudy.java
@@ -1,13 +1,15 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Array;
 import java.util.*;
 
 public class bookStudy {
-    public static void main(String [] args){
-        add("a","b","c");
+    public static void main(String[] args) {
+        add("a", "b", "c");
         add();
         add("a");
 
-        Car car1 =new Car();
+        Car car1 = new Car();
         Car car2 = new Car("red");
         Car car3 = new Car("red");
         System.out.println(car1.toString());
@@ -29,40 +31,51 @@ public class bookStudy {
         System.out.println(car2.hashCode());
         System.out.println(car3.hashCode());
 
-        String [] a = {};
-        Integer [] b={};
-        int [] c={};
-        ArrayList<String>alist= new ArrayList<>(Arrays.asList(a));
-        ArrayList<Integer>blist = new ArrayList<>(Arrays.asList(b));
-        HashSet<String>clist = new HashSet<>(alist);
+        String[] a = {};
+        Integer[] b = {};
+        int[] c = {};
+        ArrayList<String> alist = new ArrayList<>(Arrays.asList(a));
+        ArrayList<Integer> blist = new ArrayList<>(Arrays.asList(b));
+        HashSet<String> clist = new HashSet<>(alist);
+
+        Properties prop = new Properties();
+        System.out.println(Arrays.toString(args));
 
     }
-    static void add(String ...arg){
-        for(Object str : arg) {
+
+    static void add(String @NotNull ... arg) {
+        for (Object str : arg) {
             System.out.println(str);
         }
     }
 }
-class Car implements Repairable{
-    final int MAX_STATUS=100;
+
+class Car implements Repairable {
+    final int MAX_STATUS = 100;
     static int count = 0;
-    int status=100;
+    int status = 100;
     String color;
     final String num;
+
     {
-        color="white";
-        num=count+"";
+        color = "white";
+        num = count + "";
     }
-    Car(){}
-    Car(String color){
-        this.color=color;
+
+    Car() {
     }
-    void damaged(int damage){
-        this.status-=damage;
+
+    Car(String color) {
+        this.color = color;
     }
+
+    void damaged(int damage) {
+        this.status -= damage;
+    }
+
     @Override
     public void repair() {
-        this.status=MAX_STATUS;
+        this.status = MAX_STATUS;
     }
 
     @Override
@@ -88,24 +101,26 @@ class Car implements Repairable{
     }
 }
 
-class FireCar extends Car{
+class FireCar extends Car {
     String type;
-    FireCar(String type){
+
+    FireCar(String type) {
         super();
-        this.type=type;
+        this.type = type;
     }
-    void water(){
+
+    void water() {
         System.out.println("water~~");
     }
 
     @Override
     public String toString() {
-        return super.toString()+"FireCar{" +
+        return super.toString() + "FireCar{" +
                 "type='" + type + '\'' +
                 '}';
     }
 }
 
-interface Repairable{
+interface Repairable {
     void repair();
 }
